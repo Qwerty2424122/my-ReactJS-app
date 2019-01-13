a8e5eb35bf256b7d0c389e9fb97419361436691d
Bharat Bhushan Sharma <34498660+Qwerty2424122@users.noreply.github.com>
2019-01-13 13:25:37 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 class HelloWorld extends React.Component{
+    getname(){
+        return "Bharat Bhushan Sharma"
+
+    }
+    
     render(){
         return(
             <div>
             <h1>Hello</h1>
             <input type="text"></input>
             <section className="foo" >
-                Hello I am Bhaart
+                Hello I am Bharat
+                <br></br>
+                {2+2}<br/>
+                {this.getname()}
             </section>
 
             </div>
