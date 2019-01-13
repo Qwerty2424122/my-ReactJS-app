e5839985e334c5df281c895c76cb24e23868da7c
Bharat Bhushan Sharma <34498660+Qwerty2424122@users.noreply.github.com>
2019-01-13 14:06:24 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
-class HelloWorld extends React.Component{
-    getname(){
-        return "Bharat Bhushan Sharma"
-
+class TodoList extends React.Component{
+    constructor (){
+        super();
+        this.state = {
+           firstname : 'Bharat'
+        }
     }
     
     render(){
         return(
-            <div>
-            <h1>Hello</h1>
-            <input type="text"></input>
-            <section className="foo" >
-                Hello I am Bharat
-                <br></br>
-                {2+2}<br/>
-                {this.getname()}
-            </section>
-
-            </div>
-
-            
+            <ul>
+            {this.state.firstname}
+            </ul>
         )
     }
 }
 
-ReactDOM.render(<HelloWorld/>, document.getElementById('root'))
+ReactDOM.render(<TodoList/>, document.getElementById('root'))
