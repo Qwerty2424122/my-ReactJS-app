8f2cfc750b59a9bb6d315ae09ce9c3f91649c04c
Bharat Bhushan Sharma <34498660+Qwerty2424122@users.noreply.github.com>
2019-01-13 12:12:58 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class Hello extends React.Component{
+    render(){
+        return(
+            <h1>Hello</h1>
+        )
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class World extends React.Component{
+    render(){
+        return(
+            <h1>orld </h1>
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class HelloWorld extends React.Component{
+    render(){
+        return(
+            <section>
+                <Hello></Hello>
+                <World></World>
+            </section>
+        )
+    }
+
+
+}
+
+
+ReactDOM.render(<HelloWorld/>, document.getElementById('root'))
